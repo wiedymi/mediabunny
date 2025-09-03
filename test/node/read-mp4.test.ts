f13a2e6732fd0354a42c7e6e4ff74cf7617a962d
Jonny Burger <jonathanburger11@gmail.com>
2025-09-03 18:13:42 +0200

    Merge pull request #86 from JonnyBurger/url-source-allow-reading-small-files
    Do not make UrlSource fail if the size of the file is smaller than 512kb (+ add a Vitest browser mode test)

--- test/node/read-mp4.test.ts
+++ test/node/read-mp4.test.ts
@@ -1,11 +1,13 @@
 import { expect, test } from 'vitest';
-import { ALL_FORMATS, MP4, EncodedPacketSink, Input, FilePathSource } from '../src/index.js';
+import path from 'node:path';
+import { ALL_FORMATS, MP4, EncodedPacketSink, Input, FilePathSource } from '../../src/index.js';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
 test('Should be able to get packets from a .MP4 file', async () => {
+	const filePath = path.join(__dirname, '..', 'public/video.mp4');
 	const input = new Input({
-		source: new FilePathSource(`${__dirname}/files/video.mp4`),
+		source: new FilePathSource(filePath),
 		formats: ALL_FORMATS,
 	});
 
